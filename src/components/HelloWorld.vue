03ba1b553e5a1115bf1175f7550a6ea155ba072b
得利·张 <zdlhy86@163.com>
2022-04-05 16:55:11 +0800

    fix: .

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="hello">
     <h1>{{ msg }} -------- Preview</h1>
+    <h2>Title h2</h2>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
       check out the
